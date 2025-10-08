16856226aaa7d92d8d1b001311d08d4f5b950034
ngothanhtam-86-dev <thanhtam.86.dev@gmail.com>
2025-10-08 22:52:48 +0700

    Add files via upload

---
+++ C-Sharp Console App/Buoi-01_Gioi-thieu-C-Sharp/BaiTap/bt_tinhTiLe/Program.cs
@@ -0,0 +1,14 @@
+﻿// Tính tỷ lệ phần trăm
+// Yêu cầu người dùng nhập vào một số và một tổng số, sau đó tính và in ra tỷ lệ phần trăm của số đó trong tổng số
+
+// Input: số con và số tổng
+Console.Write($@"Nhập số con ");
+double soCon = Convert.ToDouble(Console.ReadLine());
+Console.Write($@"Nhập số tổng ");
+double soTong = Convert.ToDouble(Console.ReadLine());
+
+// Process
+double tinhPhanTram = soCon / soTong * 100;
+
+// Output: tính tỉ lệ % số con so với số tổng
+Console.WriteLine($@"{soCon:N0} so với {soTong:N0} chiếm {tinhPhanTram} (%)");
