12839fb89be3c3090b7780fd8cd94e048933c8d8
ngothanhtam-86-dev <thanhtam.86.dev@gmail.com>
2025-10-08 22:19:53 +0700

    Add files via upload

---
+++ C-Sharp Console App/Buoi-01_Gioi-thieu-C-Sharp/Code/Chuyen DoC sang DoF.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Viết chương trình cho phép người dùng nhập vào độ C
+// in ra màng hình độ F tương úng
+// input: doC
+Console.Write($@"Nhập vào độ C: ");
+double doC = Convert.ToDouble(Console.ReadLine());
+// process: doF = doC * 9 /5 + 32 
+double doF = doC * 9 / 5 + 32;
+// output: doF
+Console.WriteLine($@"Độ C: {doC} chuyển qua độ F là {doF}");
+
---
+++ C-Sharp Console App/Buoi-01_Gioi-thieu-C-Sharp/Code/Tinh ChuVi DienTich HinhTron.cs
@@ -0,0 +1,12 @@
+﻿// input: double pi = 3.14, bán kính
+// process: Chu Vi = 2 * pi * bán kính; diện tích = pi* bán kính * bán kính
+// output: double tinhChuVi, tinhDienTich
+double _pi = Math.PI;
+double r = 0;
+Console.Write($@"Nhập bán kính đường tròn cần tính chu vi và diện tích: ");
+r = Convert.ToDouble(Console.ReadLine());
+double tinhChuVi = 2 * _pi * r;
+double tinhDienTich = _pi * r * r;
+Console.WriteLine($@"Đường tròn có bán kính {r} có:
+Chu vi = {tinhChuVi}
+Diện tích {tinhDienTich}");
