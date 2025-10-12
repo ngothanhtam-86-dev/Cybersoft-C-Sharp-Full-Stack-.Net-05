6a58846be3e8cf5ad06e74515c18944ffbc75a9b
ngothanhtam-86-dev <thanhtam.86.dev@gmail.com>
2025-10-12 23:02:11 +0700

    Add files via upload

---
+++ C-Sharp Console App/Buoi-03-VongLap-For-while/Code/Vidu1_TongN/Program.cs
@@ -0,0 +1,48 @@
+﻿#region Ví dụ 1
+/*
+    Viết chương trình cho phép người dùng nhập vào số n (số năm làm việc)
+    Yêu cầu:
+    Tính từ tổng từ 1 đến n
+    Láy ví dụ n =5
+    tổng n = 1 +2 + 3 + 4 +5;
+    Lưu ý: không dùng công thức không sử dụng vòng lặp
+    tong = (n x (n+1))/2;    
+*/
+
+// Nhập số năm làm việc
+Console.Write("Nhập số năm làm việc (n): ");
+int n = int.Parse(Console.ReadLine());
+
+int tong = 0; // khởi tạo tổng ban đầu = 0
+
+// Dùng vòng lặp for để tính tổng
+int iFor; // khởi tạo biến đếm
+for (iFor = 1; iFor <= n; iFor++)
+{
+    tong += iFor; // cộng dồn giá trị i vào biến tong
+}
+
+// --- Vòng lặp while để tính tổng ---
+tong = 0; // set tổng lại = 0
+int iWhile = 1; // khởi tạo biến đếm
+while (iWhile <= n)
+{
+    tong += iWhile;
+    iWhile++;
+}
+
+// --- Vòng lặp do .. while để tính tổng ---
+
+tong = 0; // set tổng lại = 0
+int iDoWhile = 1; // khởi tạo biến đếm
+do
+{
+    tong += iDoWhile;
+    iDoWhile++;
+} while (iDoWhile <= n);
+
+
+// Xuất kết quả
+Console.WriteLine($"Tổng từ 1 đến {n} là: {tong}");
+
+#endregion
