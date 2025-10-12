92173ca7ee131b840b52c4a32fbd21be4d3f10c4
ngothanhtam-86-dev <thanhtam.86.dev@gmail.com>
2025-10-12 23:02:27 +0700

    Add files via upload

---
+++ C-Sharp Console App/Buoi-03-VongLap-For-while/Code/Vidu6_InBangCuuChuong/Program.cs
@@ -0,0 +1,18 @@
+﻿#region Ví dụ 6
+/*
+Bài 1: In bảng cửu chương
+▪ Yêu cầu: Viết chương trình sử dụng vòng lặp for để in bảng cửu chương của một số bất kỳ (ví dụ: 5).
+▪ Gợi ý: Yêu cầu người dùng nhập số nguyên n, sau đó sử dụng vòng lặp for để in bảng cửu chương của số đó từ 1 đến 10
+*/
+
+Console.Write("Nhập số nguyên n để in bảng cửu chương: ");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"\n--- Bảng cửu chương của {n} ---");
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine($"{n} x {i} = {n * i}");
+}
+
+
+#endregion
